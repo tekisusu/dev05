860545fe4f81558cbff4ba0eeb0fe088496f47a1
tekisusu <tekisusu@gmail.com>
2024-08-12 21:13:32 -0400

    Arreglo de Undefined a campos vacios
    Los campos vacios ya no se muestran como undefined
    ahora muestra las casillas en blanco

--- pages/script.js
+++ pages/script.js
@@ -5,7 +5,7 @@ import { mostrarModal } from "../modules/mostrarModal.js";
 import { initializeSearch } from "../modules/searchFunction.js";
 import { initScrollButtons } from "../modules/scrollButtons.js";
 import { updatePagination, currentPage, itemsPerPage } from "../modules/pagination.js";
-import { changeEstadoSelectEvent, changeRoleSelectEvent } from "../modules/tabla/changeSelectEvent.js";
+import { changeEstadoSelectEvent } from "../modules/tabla/changeSelectEvent.js";
 import { changeSemanaSelectEvent } from "../modules/tabla/changeSelectEvent/changeSemanaSelectEvent.js";
 import { changeLunesSelectEvent } from "../modules/tabla/changeSelectEvent/change1_LunesSelectEvent.js";
 import { changeMartesSelectEvent } from "../modules/tabla/changeSelectEvent/change2_MartesSelectEvent.js";
@@ -15,7 +15,6 @@ import { changeViernesSelectEvent } from "../modules/tabla/changeSelectEvent/cha
 import { changeSabadoSelectEvent } from "../modules/tabla/changeSelectEvent/change6_SabadoSelectEvent.js";
 import "../modules/downloadToExcel.js";
 import "../modules/newRegister.js";
-// import "../auth/signup_Form.js";
 
 // Constantes y variables de estado
 const tabla = document.getElementById("libreria");
@@ -49,7 +48,7 @@ export function mostrarDatos() {
 
           <td class="text-center">
             <div class="flex-container">
-              <span>${user.semana}</span>
+              <span class="${!user.semana ? 'invisible-value' : ''}">${user.semana || ''}</span>
               <select class="form-select semana-select" data-id="${user.id}">
                 <option value="---" ${user.semana === "---" ? "selected" : ""}>---</option>
                 <option value="semana 01" ${user.semana === "semana 01" ? "selected" : ""}>semana 01</option>
@@ -71,9 +70,9 @@ export function mostrarDatos() {
 
           <td class="text-center estado-col">
             <div class="flex-container">
-              <span>${user.estado}</span>
+              <span class="${!user.estado ? 'invisible-value' : ''}">${user.estado || ''}</span>
               <select class="form-select estado-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.estado === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                <option value="---" ${user.estado === "---" ? "selected" : ""}>---</option>
                 <option value="Al Día" ${user.estado === "Al Día" ? "selected" : ""}>Al Día</option>
                 <option value="Completado" ${user.estado === "Completado" ? "selected" : ""}>Completado</option>
                 <option value="Atrasado" ${user.estado === "Atrasado" ? "selected" : ""}>Atrasado</option>
@@ -83,108 +82,66 @@ export function mostrarDatos() {
 
           <td class="text-center lunes-col">
             <div class="flex-container">
-              <span>${user.lunes}</span>
+              <span class="${!user.lunes ? 'invisible-value' : ''}">${user.lunes || ''}</span>
               <select class="form-select lunes-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.lunes === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                <option value="---" ${user.lunes === "---" ? "selected" : ""}>---</option>
                 <option value="0.00" ${user.lunes === "0.00" ? "selected" : ""}>0.00</option>
                 <option value="12.00" ${user.lunes === "12.00" ? "selected" : ""}>12.00</option>
-                <option value="24.00" ${user.lunes === "24.00" ? "selected" : ""}>24.00</option>
-                <option value="36.00" ${user.lunes === "36.00" ? "selected" : ""}>36.00</option>
-                <option value="48.00" ${user.lunes === "48.00" ? "selected" : ""}>48.00</option>
-                <option value="60.00" ${user.lunes === "60.00" ? "selected" : ""}>60.00</option>
-                <option value="72.00" ${user.lunes === "72.00" ? "selected" : ""}>72.00</option>
               </select>
             </div>
           </td>
 
           <td class="text-center martes-col">
             <div class="flex-container">
-              <span>${user.martes}</span>
+              <span class="${!user.martes ? 'invisible-value' : ''}">${user.martes || ''}</span>
               <select class="form-select martes-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.martes === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                <option value="---" ${user.martes === "---" ? "selected" : ""}>---</option>
                 <option value="0.00" ${user.martes === "0.00" ? "selected" : ""}>0.00</option>
                 <option value="12.00" ${user.martes === "12.00" ? "selected" : ""}>12.00</option>
-                <option value="24.00" ${user.martes === "24.00" ? "selected" : ""}>24.00</option>
-                <option value="36.00" ${user.martes === "36.00" ? "selected" : ""}>36.00</option>
-                <option value="48.00" ${user.martes === "48.00" ? "selected" : ""}>48.00</option>
-                <option value="60.00" ${user.martes === "60.00" ? "selected" : ""}>60.00</option>
-                <option value="72.00" ${user.martes === "72.00" ? "selected" : ""}>72.00</option>
               </select>
             </div>
           </td>
 
           <td class="text-center miercoles-col">
             <div class="flex-container">
-              <span>${user.miercoles}</span>
+              <span class="${!user.miercoles ? 'invisible-value' : ''}">${user.miercoles || ''}</span>
               <select class="form-select miercoles-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.miercoles === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                <option value="---" ${user.miercoles === "---" ? "selected" : ""}>---</option>
                 <option value="0.00" ${user.miercoles === "0.00" ? "selected" : ""}>0.00</option>
                 <option value="12.00" ${user.miercoles === "12.00" ? "selected" : ""}>12.00</option>
-                <option value="24.00" ${user.miercoles === "24.00" ? "selected" : ""}>24.00</option>
-                <option value="36.00" ${user.miercoles === "36.00" ? "selected" : ""}>36.00</option>
-                <option value="48.00" ${user.miercoles === "48.00" ? "selected" : ""}>48.00</option>
-                <option value="60.00" ${user.miercoles === "60.00" ? "selected" : ""}>60.00</option>
-                <option value="72.00" ${user.miercoles === "72.00" ? "selected" : ""}>72.00</option>
               </select>
             </div>
           </td>
 
           <td class="text-center jueves-col">
             <div class="flex-container">
-              <span>${user.jueves}</span>
+              <span class="${!user.jueves ? 'invisible-value' : ''}">${user.jueves || ''}</span>
               <select class="form-select jueves-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.jueves === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                <option value="---" ${user.jueves === "---" ? "selected" : ""}>---</option>
                 <option value="0.00" ${user.jueves === "0.00" ? "selected" : ""}>0.00</option>
                 <option value="12.00" ${user.jueves === "12.00" ? "selected" : ""}>12.00</option>
-                <option value="24.00" ${user.jueves === "24.00" ? "selected" : ""}>24.00</option>
-                <option value="36.00" ${user.jueves === "36.00" ? "selected" : ""}>36.00</option>
-                <option value="48.00" ${user.jueves === "48.00" ? "selected" : ""}>48.00</option>
-                <option value="60.00" ${user.jueves === "60.00" ? "selected" : ""}>60.00</option>
-                <option value="72.00" ${user.jueves === "72.00" ? "selected" : ""}>72.00</option>
               </select>
             </div>
           </td>
 
           <td class="text-center viernes-col">
             <div class="flex-container">
-              <span>${user.viernes}</span>
+              <span class="${!user.viernes ? 'invisible-value' : ''}">${user.viernes || ''}</span>
               <select class="form-select viernes-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.viernes === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                <option value="---" ${user.viernes === "---" ? "selected" : ""}>---</option>
                 <option value="0.00" ${user.viernes === "0.00" ? "selected" : ""}>0.00</option>
                 <option value="12.00" ${user.viernes === "12.00" ? "selected" : ""}>12.00</option>
-                <option value="24.00" ${user.viernes === "24.00" ? "selected" : ""}>24.00</option>
-                <option value="36.00" ${user.viernes === "36.00" ? "selected" : ""}>36.00</option>
-                <option value="48.00" ${user.viernes === "48.00" ? "selected" : ""}>48.00</option>
-                <option value="60.00" ${user.viernes === "60.00" ? "selected" : ""}>60.00</option>
-                <option value="72.00" ${user.viernes === "72.00" ? "selected" : ""}>72.00</option>
               </select>
             </div>
           </td>
 
           <td class="text-center sabado-col">
             <div class="flex-container">
-              <span>${user.sabado}</span>
+              <span class="${!user.sabado ? 'invisible-value' : ''}">${user.sabado || ''}</span>
               <select class="form-select sabado-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.sabado === "Ninguno" ? "selected" : ""}>Ninguno</option>
+                <option value="---" ${user.sabado === "---" ? "selected" : ""}>---</option>
                 <option value="0.00" ${user.sabado === "0.00" ? "selected" : ""}>0.00</option>
                 <option value="12.00" ${user.sabado === "12.00" ? "selected" : ""}>12.00</option>
-                <option value="24.00" ${user.sabado === "24.00" ? "selected" : ""}>24.00</option>
-                <option value="36.00" ${user.sabado === "36.00" ? "selected" : ""}>36.00</option>
-                <option value="48.00" ${user.sabado === "48.00" ? "selected" : ""}>48.00</option>
-                <option value="60.00" ${user.sabado === "60.00" ? "selected" : ""}>60.00</option>
-                <option value="72.00" ${user.sabado === "72.00" ? "selected" : ""}>72.00</option>
-              </select>
-            </div>
-          </td>
-
-          <td class="text-center role-col">
-            <div class="flex-container">
-              <span>${user.role}</span>
-              <select class="form-select role-select" data-id="${user.id}">
-                <option value="Ninguno" ${user.role === "Ninguno" ? "selected" : ""}>Ninguno</option>
-                <option value="Desarrollador" ${user.role === "Desarrollador" ? "selected" : ""}>Desarrollador</option>
-                <option value="Administrador" ${user.role === "Administrador" ? "selected" : ""}>Administrador</option>
-                <option value="Participante" ${user.role === "Participante" ? "selected" : ""}>Participante</option>
               </select>
             </div>
           </td>
@@ -193,8 +150,9 @@ export function mostrarDatos() {
             <button class="btn btn-danger delete-user-button" data-id="${user.id}"><i class="bi bi-eraser-fill"></i></button>
           </td>
           
-          <td class="text-center">${user.email}</td>
-          <td class="text-center">${user.userId}</td>
+          <td class="text-center">
+            <span class="${!user.userId ? 'invisible-value' : ''}">${user.userId || ''}</span>
+          </td>
         </tr>
       `;
       tabla.innerHTML += row;
@@ -211,7 +169,6 @@ document.addEventListener('DOMContentLoaded', () => {
   mostrarDatos();
   initializeSearch(tabla);
   initScrollButtons(tabla);
-  changeRoleSelectEvent(tabla, database, collection);
   changeEstadoSelectEvent(tabla, database, collection);
   changeSemanaSelectEvent(tabla, database, collection);
   changeLunesSelectEvent(tabla, database, collection);
